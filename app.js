f9c1c12fc0f48f35d1d10023d934123a2b311ee8
ironfinger <tomhoughton888@gmail.com>
2018-04-21 23:08:00 +0100

    Weather Request Done

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+const request = require('request');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    //res.send('Hello World');
-    res.sendFile(path.join(__dirname + '/index.html'));
+// Server setup:
+var data = {
+    temp: null
+};
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    let key = '668bf60cf034c2c299111995b6d32d81';
+    let lat = '51';
+    let lng = '0';
+
+    request({
+        url: `https://api.darksky.net/forecast/${key}/${lat},${lng}`,
+        json: true
+    }, (error, res, body) => {
+        console.log(`Body of req: ${body}`);
+
+        data.temp = body.currently.temperature
+        console.log(`Pulled temp: ${data.temp}`);
+        console.log('Temperature is now assigned');
+    });
+    console.log(`Value of temp: ${data.temp}`);
+    next();
 });
 
-app.listen(3000, () => console.log('Magic Mirror listening on port 3000!')); 
+app.get('/', (req, res) => {
+    res.render('main.hbs', {
+        temperature: data.temp
+    });
+});
+
+app.listen(3000, () => console.log("App running on port 3000"));
